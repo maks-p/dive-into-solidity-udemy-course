02c2c3af916b63d9509fd3301b417e1a5b984342
maks-p <mpazuniak@gmail.com>
2022-07-18 19:00:42 -0400

    complete knowledge test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -3,11 +3,36 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract KnowledgeTest {
+    address public owner;
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address destination) public returns (bool, bytes memory){
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool success, bytes memory returnBytes) = destination.call{value: address(this).balance}(""); 
+        return (success, returnBytes);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata str1, string calldata str2) public pure returns (string memory) {
+        return string(abi.encodePacked(str1, str2));
+    }
+
+    receive() external payable {}
 }
